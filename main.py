bb2bfd6b3f74ca8a8696994a95683e2d0ae50cdb
Isaiah Ou <isaiahou@berkeley.edu>
2022-08-04 03:05:24 -0700

    make it better

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import requests
-from bs4 import BeautifulSoup
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import gspread
 import datetime
 import time
 
-url = "https://recsports.berkeley.edu/rsf-weight-room-crowd-meter/"
+from dotenv import load_dotenv
+load_dotenv()
 
-def request():
-    source = requests.get(url, "html.parser")
-    soup = BeautifulSoup(source, "lxml")
-    return soup
+service = Service(os.environ.get('CHROMEDRIVER_PATH'))
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
-def parse(soup):
+def parse():
     date = datetime.datetime.now()
-    div = soup.find_all("div",  string="% Full")
-    # for item in div:
-    #     x = item.find('span')
-    #     if x:
-    #         print(x.text.strip())
-    crowd = div
-    result = {'Date': date, "Crowd": crowd}
+    url = "https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e"
+    driver.get(url)
+    attempts = 0
+    while attempts < 3:
+        try:
+            element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div/span"))).text.removesuffix("% Full")
+            result = {'Date': date, "Crowd": element}
+            return result
+        except:
+            attempts += 1
+            element = "There was an error"
+            result = {'Date': date, "Crowd": element}
     return result
 
-# gc = gspread.service_account(filename="creds.json")
-# sh = gc.open("RSF Crowd Meter Data").sheet1
+def output(info):
+    driver.close()
+    gc = gspread.service_account(filename="creds.json")
+    sh = gc.open("RSF Crowd Meter Data").sheet1
+    sh.append_row([str(info['Date']), str(info['Crowd'])])
 
-# sh.append_row()
 
-
-data = request()
-product = parse(data)
-print(product)
-driver.close()
+info = parse()
+output(info)
