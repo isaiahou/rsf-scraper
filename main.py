c00349f028383a4108aded79cf9ba18c1c6424e3
Isaiah Ou <isaiahou@berkeley.edu>
2022-08-04 01:20:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ from bs4 import BeautifulSoup
 import gspread
 import datetime
 
+
 def request():
-    
+    source = requests.get(
+        "https://recsports.berkeley.edu/rsf-weight-room-crowd-meter/")
+    soup = BeautifulSoup(source.content, "html.parser")
+    return soup
 
 
-gc = gspread.service_account(filename="creds.json")
-sh = gc.open("RSF Crowd Meter Data").sheet1
+def parse(soup):
+    date = datetime.datetime.now()
+    div = soup.find(
+        "div", class_="styles_main__3Ul1n styles_sidebarOpen__1t7th")
+    crowd = div.span.text
+    result = {'Date': date, "Crowd": crowd}
+    return result
 
-sh.append_row()
+# gc = gspread.service_account(filename="creds.json")
+# sh = gc.open("RSF Crowd Meter Data").sheet1
+
+# sh.append_row()
+
+
+data = request()
+product = parse(data)
+print(product)
