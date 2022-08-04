b55f5e27bf09ad27f6eeb5ab28817c04f4807e9d
Isaiah Ou <isaiahou@berkeley.edu>
2022-08-04 11:54:42 -0700

    Improved formatting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import os
 import schedule
 import gspread
@@ -21,8 +20,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
-def parse():
-    date = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+def parse(date):
     url = "https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e"
     driver.get(url)
     attempts = 0
@@ -30,24 +28,32 @@ def parse():
         try:
             element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
                 (By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div/span"))).text.removesuffix("% Full")
-            result = {'Date': date, "Crowd": element}
+            result = {"Month": date.strftime("%B"), "Day": date.strftime("%A"), "Time": date.strftime("%I:%M %p"), "Crowd": element}
             return result
         except:
             attempts += 1
             element = "There was an error"
-            result = {'Date': date, "Crowd": element}
+            result = {"Month": date.strftime("%B"), "Day": date.strftime("%A"), "Time": date.strftime("%I:%M %p"), "Crowd": element}
     return result
+
+def valid_time(date):
+    timestamp = date.time()
+    start = datetime.time(7)
+    end = datetime.time(22)
+    return (start <= timestamp <= end)
 
 
 def output(info):
     gc = gspread.service_account(filename="creds.json")
     sh = gc.open("RSF Crowd Meter Data").sheet1
-    sh.append_row([str(info['Date']), str(info['Crowd'])])
+    sh.append_row([str(info['Month']), str(info['Day']), str(info['Time']), str(info['Crowd'])])
 
 
 def execute():
-    info = parse()
-    output(info)
+    date = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+    if valid_time(date):
+        info = parse(date)
+        output(info)
 
 schedule.every().hour.at(":00").do(execute)
 schedule.every().hour.at(":30").do(execute)
